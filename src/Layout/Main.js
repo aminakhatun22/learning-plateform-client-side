7124ef65bb81c990a834e312fe7408b609ddc8b6
aminakhatun22 <aminak26802@gmail.com>
2022-10-25 14:39:21 +0600

    added blog

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
+import Footer from '../Pages/Shared/Footer/Footer';
+import Header from '../Pages/Shared/Header/Header';
+// import { Outlet } from 'react-router-dom';
 const Main = () => {
     return (
         <div>
-            <Outlet></Outlet>
+            <Header></Header>
+            <Container>
+                <Row>
+                    <Col lg="4">
+                        <h3>Left side</h3>
+
+                    </Col>
+
+                    <Col lg="8">
+                        {/* <Outlet></Outlet> */}
+                        <h2>
+                            Right side
+                        </h2>
+                    </Col>
+                </Row>
+            </Container>
+
+            <Footer></Footer>
         </div>
     );
 };
