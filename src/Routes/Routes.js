2b2dbe522ebf5ef1e4845685c728cfe1b57ce4e5
aminakhatun22 <aminak26802@gmail.com>
2022-10-27 09:44:28 +0600

    done all route

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,20 +2,23 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Authentication from "../Pages/Authentication/Authentication";
 import Blog from "../Pages/Blog/Blog";
+import Checkout from "../Pages/Checkout/Checkout";
 import Courses from "../Pages/Courses/Courses";
 import About from "../Pages/Courses/RightSide/About";
 import MyCourses from "../Pages/Courses/RightSide/MyCourses";
 import Payment from "../Pages/Courses/RightSide/Payment";
 import Profile from "../Pages/Courses/RightSide/Profile";
-import ErrorPage from "../Pages/ErrorPage";
-import FAQ from "../Pages/FAQ/FAQ";
+import SingleCourse from "../Pages/Courses/RightSide/SingleCourse";
 import Home from "../Pages/Home/Home";
+import PrivateRoute from "../Pages/PrivateRoute/PrivateRoute";
+
+//server url
+const url = 'http://localhost:4000';
 
 export const routes = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
-        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/home',
@@ -30,8 +33,18 @@ export const routes = createBrowserRouter([
                 element: <Blog />,
             },
             {
-                path: '/faq',
-                element: <FAQ />,
+                path: '/checkout/:id',
+                loader: async ({ params }) => {
+                    return fetch(`${url}/courses/${params.id}`)
+                },
+                element: <PrivateRoute><Checkout /></PrivateRoute>,
+            },
+            {
+                path: '/course/:id',
+                loader: async ({ params }) => {
+                    return fetch(`${url}/courses/${params.id}`)
+                },
+                element: <SingleCourse />,
             },
             {
                 path: '/courses',
@@ -39,7 +52,10 @@ export const routes = createBrowserRouter([
                 children: [
                     {
                         index: true,
-                        element: <MyCourses />
+                        element: <MyCourses />,
+                        loader: async () => {
+                            return fetch(`${url}/courses`)
+                        }
                     },
                     {
                         path: 'profile',
